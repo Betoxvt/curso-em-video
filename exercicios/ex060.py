a92b9edf062d8dff58e85eb091201331e862a697
Betoxvt <robertoguedes@edu.univali.br>
2024-07-21 13:28:59 -0300

    CALCULO DO FATORIAL

--- exercicios/ex060.py
+++ exercicios/ex060.py
@@ -3,34 +3,30 @@
 # Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
 print('Este programa mostra o fatorial de um número')
 n = int(input('Insira o valor: '))
-fat = n
+fat = 1
 
 # Usando while
-if n > 1:
+if n > 0:
     print(f'{n}! =', end=' ')
-    while n != 1:
-        print(f'{n}', end=' X ')
-        fat = fat * (n-1)
-        n += -1
-    print(f'1 = {fat}')
-elif n == 1:
-    print('1! = 1')
+    while n > 0:
+        print(f'{n}', end='')
+        print(' x ' if n > 1 else ' = ', end='')
+        fat *= n
+        n -= 1
+    print(f'{fat}')
 elif n == 0:
     print('0! = 1')
 elif n < 0:
     print('Não há fatorial de números negativos')
-
-# Usando for
 '''
-if n > 1:
+# Usando for
+if n > 0:
     print(f'{n}! =', end=' ')
-    for n in range(n, 1, -1):
-        if n > 1:
-            fat = fat*(n-1)
-            print(f'{n}', end=' X ')
-    print('1 = {}'.format(fat))
-elif n == 1:
-    print('1! = 1')
+    for n in range(n, 0, -1):
+        print(f'{n}', end='')
+        print(' x ' if n > 1 else ' = ', end='')
+        fat *= n
+    print(fat)
 elif n == 0:
     print('0! = 1')
 elif n < 0:
