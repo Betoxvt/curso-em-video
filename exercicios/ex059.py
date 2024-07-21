d1eb4febeaca4c6bfdcf976eca72d512bb8228fd
Betoxvt <robertoguedes@edu.univali.br>
2024-07-21 11:17:39 -0300

    CRIANDO UM MENU DE OPÇÕES

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -6,9 +6,11 @@
 # [4] novos números
 # [5] sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso.
-
+from time import sleep
+print('Insira os números para o programa.')
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
+sleep(0.5)
 opt = 0
 while opt != 5:
     print('-------- MENU ---------')
@@ -18,19 +20,33 @@ while opt != 5:
     print('[4] Novos Números')
     print('[5] Sair do Programa')
     print('-----------------------')
-    opt = int(input('Escolha: '))
+    sleep(0.5)
+    opt = int(input('Escolha uma das opções: '))
     if opt == 1:
+        sleep(0.5)
         print('A soma entre {} e {} é {}'.format(n1, n2, n1 + n2))
+        sleep(3)
     if opt == 2:
+        sleep(0.5)
         print('A multiplicação entre {} e {} é {}'.format(n1, n2, n1 * n2))
+        sleep(3)
     if opt == 3:
+        sleep(0.5)
         if n1 == n2:
             print('Os números {} são iguais'.format(n1))
+            sleep(3)
         if n1 > n2:
             print('{} é o MAIOR.'.format(n1))
+            sleep(3)
         if n2 > n1:
             print('{} é o MAIOR.'.format(n2))
+            sleep(3)
     if opt == 4:
+        sleep(0.5)
+        print('Inserir novos números para o programa.')
+        sleep(0.5)
         n1 = int(input('Primeiro valor: '))
         n2 = int(input('Segundo valor: '))
+        sleep(0.5)
+sleep(0.5)
 print('Fim do programa')
