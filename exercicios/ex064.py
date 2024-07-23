816438b091558505b965aaa4c9f913e398bc44e3
Betoxvt <robertoguedes@edu.univali.br>
2024-07-23 15:28:00 -0300

    VALIDANDO DADOS

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -1,14 +1,12 @@
 # Crie um programa que leia vários números inteiros pelo teclado.
 # O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 # No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
-n = 0
-c = 0
-s = 0
+n = c = s = 0
 print('Calcular a soma entre vários números')
 print('Digite números inteiros ou 999 para parar')
 while n != 999:
     n = int(input('>>>> '))
-    if n != 999:
-        s += n
+    if n != 999:  # Falaram que com esse if consome mais memória
+        s += n    # O jeito é chamar o n também antes do while
         c += 1
 print(f'{c} números digitados e a soma entre eles da {s}.')
