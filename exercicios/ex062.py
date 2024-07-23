8d03ed0553093858f017e22242b2d728e5011170
Betoxvt <robertoguedes@edu.univali.br>
2024-07-23 15:25:31 -0300

    PROGRESSÃO ARITMÉTICA V3.0

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,24 +1,20 @@
 # Melhore o ex061, perguntando para o usuário se ele quer mostrar mais alguns termos.
 # O programa encerra quando ele disser que quer mostrar 0 termos.
+print('Gerador de PA')
+print('-=-' * 6)
 p = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
 c = 1
-f = 10
-m = 1
+f = 0
+m = 10
 print('Os dez primeiros termos da P.A. são:')
 while m != 0:
-    while c < f:
+    f = f + m
+    while c <= f:
         print(p, end=' → ')
         p += r
         c += 1
-    print(p)
-    print('Insira [0] para sair do programa\nOu')
+    print('PAUSA')
     m = int(input('Quantos termos a mais: '))
-    if m > 0:
-        print('Continuando a partir do último termo...')
-        f += m
-    elif m < 0:
-        f -= m
-        print('Vou considerar como positivo.')
-        print('Continuando a partir do último termo...')
+print(f'P.A. com um total de {f} termos.')
 print('Fim do programa')
