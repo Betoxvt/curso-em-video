a872b824f519593d7d1f1e3db0234aee17c923ac
Betoxvt <robertoguedes@edu.univali.br>
2024-07-23 15:26:29 -0300

    SEQUÊNCIA FIBONACCI

--- exercicios/ex063.py
+++ exercicios/ex063.py
@@ -2,17 +2,32 @@
 # Escreva um programa que leia um número inteiro qualquer
 # e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci.
 # Ex: 0 → 1 → 1 → 2 → 3 → 5 → 8
+
+print('-=-' * 10)
+e = int(input('Digite o nº de elementos: ').center(30))
+print('-=-' * 10)
 c = 1
-e = int(input('Digite o nº de elementos: '))
 na = 0
 nb = 1
 nc = 0
-print(f'Os {e} primeiros elementos da sequência de Fibonacci são:')
-print(0, end=' → ')
-while c < e - 1:
-    print(nb, end=' → ')
-    c += 1
-    na = nb
-    nb = nc + na
-    nc = nb - nc
-print(nb)
+if e == 0:
+    print('Ok nenhum elemento então')
+if e == 1:
+    print('O primeiro elemento da sequência de Fibonacci é:')
+    print('~~' * 30,
+          '\n0')
+if e > 1:
+    print(f'A Sequência de Fibonacci com {e} elementos é:')
+    print('~~' * 30)
+    print(0, end=' → ')
+    while c < e - 1:
+        print(nb, end=' → ')
+        c += 1
+        na = nb
+        nb = nc + na
+        nc = nb - nc
+    print(nb)
+if e < 0:
+    (print('Por favor use números inteiros e positivos de preferência'))
+print('~~' * 30)
+print('FIM')
