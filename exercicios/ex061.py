bd4deaeec3b0427de7a473c7dc562233725198b6
Betoxvt <robertoguedes@edu.univali.br>
2024-07-23 15:24:23 -0300

    PROGRESSÃO ARITMÉTICA V2.0

--- exercicios/ex061.py
+++ exercicios/ex061.py
@@ -1,12 +1,15 @@
 # PROGRESSÃO ARITMÉTICA v2.0
 # Refaça o ex051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 # usando a estrutura while.
+print('Gerador de PA')
+print('-=-' * 6)
 p = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
-c = 0
+c = 1
 print('Os dez primeiros termos da P.A. são:')
-while c != 9:
+while c <= 9:
     print(p, end=' → ')
     p += r
     c += 1
 print(p)
+print('FIM')
