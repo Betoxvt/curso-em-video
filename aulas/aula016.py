7efadb677fe452f534a88a1b13b7679e658e4a9d
Betoxvt <robertoguedes@edu.univali.br>
2024-07-24 17:00:09 -0300

    Variaveis compostas - Tupla

--- aulas/aula016.py
+++ aulas/aula016.py
@@ -3,16 +3,16 @@
 # Os valores identificados por indices [0] [1] [2] [3] > regras de fatiamento
 # As tuplas são IMUTÁVEIS!
 lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim')  # () para tuplas! ou sem também funciona
-print(lanche)       # ('Hamburger', 'Suco', 'Pizza', 'Pudim')
-print(lanche[1])    # Suco
-print(lanche[-1])   # Pudim
-print(lanche[-2])   # Pizza
+print(lanche)  # ('Hamburger', 'Suco', 'Pizza', 'Pudim')
+print(lanche[1])  # Suco
+print(lanche[-1])  # Pudim
+print(lanche[-2])  # Pizza
 print(lanche[1:3])  # ('Suco', 'Pizza')
-print(lanche[2:])   # ('Pizza', 'Pudim')
-print(lanche[:2])   # ('Hamburger', 'Suco')
+print(lanche[2:])  # ('Pizza', 'Pudim')
+print(lanche[:2])  # ('Hamburger', 'Suco')
 print(lanche[-2:])  # ('Pizza', 'Pudim')
 print(lanche[::2])  # ('Hamburger', 'Pizza')
-print(lanche[::-1]) # ('Pudim', 'Pizza', 'Suco', 'Hamburger')
+print(lanche[::-1])  # ('Pudim', 'Pizza', 'Suco', 'Hamburger')
 for comida in lanche:
     print(f'Eu vou comer {comida}')
 print('Comi pra caramba!')
@@ -21,16 +21,16 @@ for cont in range(0, len(lanche)):
     print(f'Eu vou comer {lanche[cont]} na posição {cont}')
 for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos}')
-print(sorted(lanche))   # Mostra o lanche em ordem alfabética, exibe uma lista daí.
+print(sorted(lanche))  # Mostra o lanche em ordem alfabética, exibe uma lista daí.
 a = (2, 5, 4)
 b = (5, 8, 1, 2)
-c = a + b           # cria uma tupla com os elementos na ordem 1º de a depois b
+c = a + b  # cria uma tupla com os elementos na ordem 1º a depois b
 print(c)
 print(len(c))
-print(c.count(5))   # contagem de '5'
-print(c.index(8))   # posição do 1º 8
-print(c.index(5, 1))    # posição de 5 a partir da 1
-pessoa = ('Gustavo', 39, 'M', 99.88)    # Aceita diferentes tipos de variáveis
+print(c.count(5))  # contagem de '5'
+print(c.index(8))  # posição do 1º 8
+print(c.index(5, 1))  # posição de 5 a partir da 1
+pessoa = ('Gustavo', 39, 'M', 99.88)  # Aceita diferentes tipos de variáveis
 print(pessoa)
-del pessoa      # Podemos apagar a tupla
-print(pessoa)   # Aí não imprime mais
+del pessoa  # Podemos apagar a tupla
+print(pessoa)  # Aí não imprime mais
