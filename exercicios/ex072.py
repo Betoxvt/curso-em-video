ad0366c636a5182f60fba7c3c7f11901ea64d6c6
Betoxvt <robertoguedes@edu.univali.br>
2024-07-25 18:04:48 -0300

    Número por extenso

---
+++ exercicios/ex072.py
@@ -0,0 +1,7 @@
+# Número por extenso
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+extensos = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+            'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+num = int(input('Digite um número inteiro: '))
+print(f'O número digitado por extenso é {extensos[num]}')
