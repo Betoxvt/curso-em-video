707a33fc2ec8bc4192153fe9b9703e862e4af3eb
Betoxvt <robertoguedes@edu.univali.br>
2024-07-26 15:01:59 -0300

    Larger and lesser values in tuple

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -2,6 +2,15 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 from random import randint
+# Class method
+numbers = (randint(1, 10), randint(1, 10), randint(1, 10),
+           randint(1, 10), randint(1, 10))
+print('The numbers drawn were: ', end='')
+for n in numbers:
+    print(f'{n} ', end='')
+print(f'\nThe largest number drawn was: {max(numbers)}')
+print(f'The smallest number drawn was: {min(numbers)}')
+'''
 n0 = randint(0, 9)
 n1 = randint(0, 9)
 n2 = randint(0, 9)
@@ -11,3 +20,4 @@ tup = (n0, n1, n2, n3, n4)
 print(tup)
 print(f'The greater number is {sorted(tup)[-1]}.')
 print(f'And the lesser number is {sorted(tup)[0]}.')
+'''
