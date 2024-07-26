7023e4a406c6420d8f7679091726d3eb3deb0c6e
Betoxvt <robertoguedes@edu.univali.br>
2024-07-26 11:46:10 -0300

    Número por extenso

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -3,5 +3,15 @@
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 extensos = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
             'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-num = int(input('Digite um número inteiro: '))
-print(f'O número digitado por extenso é {extensos[num]}')
+while True:
+    while True:
+        num = int(input('Digite um número inteiro entre 0 e 20: '))
+        if 0 <= num <= 20:
+            break
+    print(f'O número digitado por extenso é {extensos[num]}')
+    resp = ' '
+    while resp not in 'SN':
+        resp = input('Quer continuar? [S/N] ').upper().strip()[0]
+    if resp == 'N':
+        break
+print('Programa terminado')
