a1df539896a20974e034059dcef2ac8db76baaf9
Betoxvt <robertoguedes@edu.univali.br>
2024-07-26 15:28:10 -0300

    Price list with tuple

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -3,11 +3,20 @@
 # No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 tup = ('Mouse', 66.99, 'Keyboard', 89.90, 'Monitor', 480, 'Case', 330, 'Laptop', 5399, 'WebCam', 180,
        'Mouse Pad', 12.50, 'Power Supply Unit', 205.6)
+print('—'*40)
+print(f'{'Price List':^40}')
+print('—'*40)
+# Class method
+for pos in range(0, len(tup)):
+    if pos % 2 == 0:
+        print(f'{tup[pos]:.<30}', end='')
+    else:
+        print(f'R${tup[pos]:>8.2f}')
+print('—' * 40)
+'''
 c = 0
-print('-'*40)
-print(f'{'Tabela de produtos':^40}')
-print('='*40)
 while c < len(tup)-1:
     valor = str(f'R$ {tup[c+1]:.2f}').replace('.', ',')  # Changed dot for comma just for practice,
     print(f'{tup[c]:.<20}{valor:.>20}')                               # to fit the brazilian standard.
     c += 2
+'''
