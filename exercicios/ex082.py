26eb825932f30ead45a8e8d85ef13af9ff195ec9
Betoxvt <robertoguedes@edu.univali.br>
2024-07-29 12:04:45 -0300

    Dividing values in multiple lists

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -21,3 +21,22 @@ for n in numbers:
         odd_numbers.append(n)
 print(f'The even numbers list is {even_numbers}')
 print(f'The odd numbers list is {odd_numbers}')
+
+# Teacher's solution
+num = list()
+pares = list()
+ímpares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        ímpares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {ímpares}')
