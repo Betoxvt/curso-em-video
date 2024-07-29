8e52870759d6fa81e425685e92ec89f344598eb9
Betoxvt <robertoguedes@edu.univali.br>
2024-07-29 11:49:59 -0300

    List sorted directly from input

--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -20,4 +20,23 @@ for i in range(0, 5):
     elif _list[2] <= v < _list[3]:
         _list.insert(3, v)
         print('Added at position 3.')
-print(_list)
+print('—' * 50)
+print(f'The list in ascending order is {_list} (without sort())')
+
+# Teacher’s solution
+lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > _list[-1]:
+        lista.append(n)
+        print('Adicionado ao final da lista.')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista.')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
