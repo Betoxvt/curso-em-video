6f694cebac009b7683036adfb29d4bbeb400fdc3
Betoxvt <robertoguedes@edu.univali.br>
2024-08-01 16:46:48 -0300

    Dictionary

---
+++ exercicios/ex090.py
@@ -0,0 +1,14 @@
+# Dictionary
+# Create a program that reads name and average from a student, saving also his situation in a dictionary.
+# At the end, show the content of the structure on screen.
+student = {'Name': input('Name: ')}
+student['Average'] = float(input(f'{student["Name"]} Average: '))
+if student['Average'] >= 7:
+    student['Situation'] = 'Passed'
+elif 5 <= student['Average'] < 7:
+    student['Situation'] = 'on probation'
+else:
+    student['Situation'] = 'Failed'
+
+for k, v in student.items():
+    print(k, 'is', v)
