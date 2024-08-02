a93a5e7643e84de02f546ad1c9d2e6b8b78c1050
Betoxvt <robertoguedes@edu.univali.br>
2024-08-02 14:34:16 -0300

    Dice Game

--- exercicios/ex091.py
+++ exercicios/ex091.py
@@ -3,6 +3,7 @@
 # At the end, put this dictionary in order, knowing that the winner got the highest dice throw number.
 from random import randint
 from time import sleep
+from operator import itemgetter  # Needed in teacher's solution
 turn = []
 input('Throw the dice! [ENTER]')
 sleep(1)
@@ -18,3 +19,18 @@ for i, v in enumerate(turn, start=1):
 print(f'Congratulations! Player {turn[0]['Player']} wins!')
 
 # Later, in case of a tie, a tie-breaking procedure will be implemented.
+
+# Teacher's Solution
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = list()
+print('Valores sorteados:')
+for k, v in jogo.items():
+    print(f'{k} tirou {v}')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} tirou {v[1]}.')
+    sleep(1)
