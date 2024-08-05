fbc779dd90bcaf9023a3be2359a3c96674068c33
Betoxvt <robertoguedes@edu.univali.br>
2024-08-05 19:42:49 -0300

    Land Area calculator function

---
+++ exercicios/ex096.py
@@ -0,0 +1,8 @@
+# Function - Area calculator
+# Create a program that has a function called area(), receiving a rectangular land dimensions and show the land area.
+def area(length, width):
+    area = length * width
+    print(f'The area of a land {length} X {width} is {area}m²')
+
+
+area(int(input('Length (m): ')), int(input('Width (m): ')))
