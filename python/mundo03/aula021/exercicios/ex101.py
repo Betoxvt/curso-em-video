1bac0c3a2244de47e7374f32c7eb29d661b47f1c
Roberto Guedes <robertoguedes@edu.univali.br>
2024-08-12 23:11:13 -0300

    Voting function

--- python/mundo03/aula021/exercicios/ex101.py
+++ python/mundo03/aula021/exercicios/ex101.py
@@ -1,18 +1,16 @@
 # Create a program which have a function named vote(), it shall receive as a parâmeter a persons year of birth,
 # returning a literal value indicating whether a person has mandatory, optional ou denied vote at the elections.
-from datetime import date
-
-
 def vote(year_of_birth):
-    age_now = date.today().year - year_of_birth
-    if age_now < 16:
-        return 'DENIED'
-    elif age_now > 70 or age_now < 18:
-        return 'OPTIONAL'
+    from datetime import date
+    age = date.today().year - year_of_birth
+    if age < 16:
+        return f'With {age} years: voting is DENIED.'
+    elif age > 70 or age < 18:
+        return f'With {age} years: voting is OPTIONAL.'
     else:
-        return 'MANDATORY'
+        return f'With {age} years: voting is MANDATORY.'
 
 
-person = int(input('Please inform year of birth: '))
-age = date.today().year - person
-print(f'At age {age} vote is {vote(person)}')
+# Main code
+birth = int(input('Please inform year of birth: '))
+print(vote(birth))
