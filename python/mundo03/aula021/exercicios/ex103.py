14b178e05de64a2d5459f07b75bf7ac2db6a0fd9
Roberto Guedes <robertoguedes@edu.univali.br>
2024-08-14 16:12:21 -0300

    Players record

---
+++ python/mundo03/aula021/exercicios/ex103.py
@@ -0,0 +1,32 @@
+# Players record
+# Do a program with the function record(), it receives two optional parameters: players name and how many goals he scored. The program must be able to show the players record, even if some data is missing or incorrect.
+def record(name='', goals=''):
+    if name.strip() == '':
+        name = "<unknown>"
+    if goals.isnumeric() == False:
+        goals = "0"
+    print(f"Player {name} scored {goals} goal(s) in the tournament.")
+    
+ 
+# Main program
+record(str(input("Player's name: ")), str(input("Player's score: ")))
+
+# Teacher Guanabara solution (transforms goals into integer number)
+
+# def ficha(jog='<desconhecido>', gol=0):
+#     print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
+
+
+
+# Programa principal
+# n = str(input("Nome do Jogador: "))
+# g = str(input("Número de Gols: "))
+# if g.isnumeric():
+#     g = int(g)
+# else:
+#     g = 0
+# if n.strip() == '':
+#     ficha(gol=g)
+# else:
+#     ficha(n, g)
+
